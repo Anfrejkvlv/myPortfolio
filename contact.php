e3263847b8450aaf4fd39157afb52ac9ba84aba0
Anfrejkvlv <eloubahasra@gmail.com>
2025-03-30 09:12:38 +0000

    Mise à jour

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     <section class="confirmation-section">
         <div class="confirmation-container">
             <?php
-            include 'send_mail.php';
+            //include 'send_mail.php';
             global $name, $email, $message, $conn;
             include 'database/DatabaseCreat.php';
 
@@ -12,13 +12,13 @@
                 $email = htmlspecialchars($_POST["email"]);
                 $message = htmlspecialchars($_POST["message"]);
 
-                // Insertion des données dans la base de données
+
                 $req = "INSERT INTO contact (Nom_prenom, email, message) VALUES (?, ?, ?)";
                 $stm = $conn->prepare($req);
                 $stm->bind_param("sss", $name, $email, $message);
                 $stm->execute();
 
-                // Affichage du message de confirmation stylé
+
                 echo '<div class="confirmation-message">';
                 echo '<h1>Merci, ' . $name . ' !</h1>';
                 echo '<p>Votre message a été envoyé avec succès :</p>';
